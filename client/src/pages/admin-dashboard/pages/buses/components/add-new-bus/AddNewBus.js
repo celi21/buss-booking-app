63781acec4be2fab92fef0c277615f45c96cff23
joharkhan99 <joharkhan1999@gmail.com>
2024-08-31 16:21:46 +0500

    Added new bus form

--- client/src/pages/admin-dashboard/pages/buses/components/add-new-bus/AddNewBus.js
+++ client/src/pages/admin-dashboard/pages/buses/components/add-new-bus/AddNewBus.js
@@ -1,22 +1,156 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchRoutes } from "../../../../../../store/slices/RoutesSlice";
 import { fetchBusTypes } from "../../../../../../store/slices/BusTypeSlice";
 import { ArrowDown } from "react-bootstrap-icons";
+import LoadingSpinner from "../../../../../../components/loading-spinner/LoadingSpinner";
 
 const AddNewBus = ({ handleClose, show }) => {
-  const handleSubmit = () => {};
-  const { routes } = useSelector((state) => state.routes);
+  const { routes, isRoutesLoading } = useSelector((state) => state.routes);
   const { busTypes } = useSelector((state) => state.busType);
+  const [selectedRoute, setSelectedRoute] = useState(null);
+  const [selectedBusType, setSelectedBusType] = useState(null);
+  const [periodOperatingFrom, setPeriodOperatingFrom] = useState(null);
+  const [periodOperatingTo, setPeriodOperatingTo] = useState(null);
+  const [recurring, setRecurring] = useState([
+    { id: 1, name: "Monday", checked: false },
+    { id: 2, name: "Tuesday", checked: false },
+    { id: 3, name: "Wednesday", checked: false },
+    { id: 4, name: "Thursday", checked: false },
+    { id: 5, name: "Friday", checked: false },
+    { id: 6, name: "Saturday", checked: false },
+    { id: 7, name: "Sunday", checked: false },
+  ]);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchRoutes());
     dispatch(fetchBusTypes());
   }, []);
 
+  const handleRouteChange = (e) => {
+    if (e.target.value) {
+      let findRoute = routes.find((r) => r._id == e.target.value);
+      if (findRoute) {
+        // map over locations and add arrival and departure time to null
+        let updatedLocations = findRoute.locations.map((location, index) => {
+          if (index == 0) {
+            return {
+              ...location,
+              departureTime: null,
+            };
+          } else if (index == findRoute.locations.length - 1) {
+            return {
+              ...location,
+              arrivalTime: null,
+            };
+          } else {
+            return {
+              ...location,
+              arrivalTime: null,
+              departureTime: null,
+            };
+          }
+        });
+
+        setSelectedRoute({
+          ...findRoute,
+          locations: updatedLocations,
+        });
+      }
+    } else {
+      setSelectedRoute(null);
+    }
+  };
+
+  const formatTime = (time) => {
+    let timeSplit = time.split(":"); // hours:minutes
+    let hours = timeSplit[0];
+    let minutes = timeSplit[1];
+    let meridian = "";
+    if (hours > 12) {
+      meridian = "PM";
+      hours -= 12;
+    } else if (hours < 12) {
+      meridian = "AM";
+      if (hours == 0) {
+        hours = 12;
+      }
+    } else {
+      meridian = "PM";
+    }
+    return `${hours}:${minutes}:${meridian}`;
+  };
+
+  const handleTimeChange = (e, locId, index, type) => {
+    if (!e.target.value || !selectedRoute || !locId) {
+      console.log("object");
+      return;
+    }
+    let time = formatTime(e.target.value);
+
+    let updatedLocations = selectedRoute.locations.map((loc, index) => {
+      if (loc._id === locId) {
+        if (type == "departure") {
+          return {
+            ...loc,
+            departureTime: time,
+          };
+        } else if (type == "arrival") {
+          return {
+            ...loc,
+            arrivalTime: time,
+          };
+        }
+      } else {
+        return loc;
+      }
+    });
+    setSelectedRoute({
+      ...selectedRoute,
+      locations: updatedLocations,
+    });
+  };
+
+  const handleBusTypeChange = (e) => {
+    if (!e.target.value) {
+      setSelectedBusType(null);
+      return;
+    }
+    setSelectedBusType(e.target.value);
+  };
+
+  const handleRecurringCheckbox = (e, day) => {
+    if (!day) return;
+    let updatedRecurring = recurring.map((d) => {
+      if (d.id == day.id) {
+        return {
+          ...d,
+          checked: !d.checked,
+        };
+      }
+      return d;
+    });
+
+    setRecurring(updatedRecurring);
+  };
+
+  const handleSubmit = () => {
+    console.log(selectedRoute, "submit");
+    console.log(recurring);
+  };
+
   return (
-    <Modal show={show} onHide={handleClose} centered size="lg">
+    <Modal
+      show={show}
+      onHide={() => {
+        setSelectedRoute(null);
+        handleClose();
+      }}
+      centered
+      size="lg"
+    >
       <Modal.Header closeButton>
         <Modal.Title>Add New Bus</Modal.Title>
       </Modal.Header>
@@ -24,7 +158,13 @@ const AddNewBus = ({ handleClose, show }) => {
       <Modal.Body>
         <div className="mb-3 d-flex justify-content-start align-items-center flex-row gap-2">
           <div className="w-50 fw-semibold">Route:</div>
-          <select className="form-control">
+          <select
+            className="form-control"
+            onChange={(e) => handleRouteChange(e)}
+          >
+            <option value="" key="">
+              Choose a Route
+            </option>
             {routes.map((route) => (
               <option value={route._id} key={route._id}>
                 {route.name}
@@ -35,7 +175,15 @@ const AddNewBus = ({ handleClose, show }) => {
 
         <div className="mb-3 d-flex justify-content-start align-items-center flex-row gap-2">
           <div className="w-50 fw-semibold">Bus Type:</div>
-          <select className="form-control">
+          <select
+            className="form-control"
+            onChange={(e) => {
+              handleBusTypeChange(e);
+            }}
+          >
+            <option value="" key="">
+              Choose a Bus Type
+            </option>
             {busTypes.map((busType) => (
               <option value={busType._id} key={busType._id}>
                 {busType.name}, {busType.seats} seat(s)
@@ -44,67 +192,98 @@ const AddNewBus = ({ handleClose, show }) => {
           </select>
         </div>
 
-        <div className="d-flex flex-row align-items-center mb-3">
-          <div style={{ width: "120px" }} className="fw-semibold">
-            Route Details
+        <hr />
+
+        <div className="mb-3">
+          <div className="fw-semibold">Route Details</div>
+          <div className="d-flex justify-content-center">
+            <small>
+              <i>{selectedRoute == null && "Please Choose a Route"}</i>
+            </small>
           </div>
-          <div
-            className="w-100"
-            style={{ height: 1, background: "#ddd" }}
-          ></div>
         </div>
 
-        {routes[1].locations.map((location, index) => {
-          if (index == 0) {
-            return (
-              <>
+        {selectedRoute &&
+          selectedRoute.locations.map((location, index) => {
+            if (index == 0) {
+              return (
+                <>
+                  <div className="d-flex justify-content-start align-items-center flex-row gap-2">
+                    <div className="w-50">{location.name}</div>
+                    <div className="w-100 d-flex align-items-center flex-row">
+                      <div className="w-50">Departure Time:</div>
+                      <input
+                        type="time"
+                        className="form-control"
+                        onChange={(e) => {
+                          handleTimeChange(e, location._id, index, "departure");
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className="d-flex flex-column align-items-start">
+                    <ArrowDown size={30} className="my-2" />
+                  </div>
+                </>
+              );
+            } else if (index === selectedRoute.locations.length - 1) {
+              return (
                 <div className="d-flex justify-content-start align-items-center flex-row gap-2">
                   <div className="w-50">{location.name}</div>
                   <div className="w-100 d-flex align-items-center flex-row">
-                    <div className="w-50">Departure Time:</div>
-                    <input type="time" className="form-control" />
+                    <div className="w-50">Arrival Time:</div>
+                    <input
+                      type="time"
+                      className="form-control"
+                      onChange={(e) => {
+                        handleTimeChange(e, location._id, index, "arrival");
+                      }}
+                    />
                   </div>
                 </div>
-                <div className="d-flex flex-column align-items-start">
-                  <ArrowDown size={30} className="my-2" />
-                </div>
-              </>
-            );
-          } else if (index === routes[1].locations.length - 1) {
-            return (
-              <div className="d-flex justify-content-start align-items-center flex-row gap-2">
-                <div className="w-50">{location.name}</div>
-                <div className="w-100 d-flex align-items-center flex-row">
-                  <div className="w-50">Arrival Time:</div>
-                  <input type="time" className="form-control" />
-                </div>
-              </div>
-            );
-          } else {
-            return (
-              <>
-                <div className="d-flex justify-content-start align-items-center flex-row gap-2">
-                  <div className="w-50">{location.name}</div>
+              );
+            } else {
+              return (
+                <>
+                  <div className="d-flex justify-content-start align-items-center flex-row gap-2">
+                    <div className="w-50">{location.name}</div>
 
-                  <div className="d-flex flex-column justify-items-center w-100 gap-2">
-                    <div className="w-100 d-flex align-items-center flex-row">
-                      <div className="w-50">Arrival Time:</div>
-                      <input type="time" className="form-control" />
-                    </div>
+                    <div className="d-flex flex-column justify-items-center w-100 gap-2">
+                      <div className="w-100 d-flex align-items-center flex-row">
+                        <div className="w-50">Arrival Time:</div>
+                        <input
+                          type="time"
+                          className="form-control"
+                          onChange={(e) => {
+                            handleTimeChange(e, location._id, index, "arrival");
+                          }}
+                        />
+                      </div>
 
-                    <div className="w-100 d-flex align-items-center flex-row">
-                      <div className="w-50">Departure Time:</div>
-                      <input type="time" className="form-control" />
+                      <div className="w-100 d-flex align-items-center flex-row">
+                        <div className="w-50">Departure Time:</div>
+                        <input
+                          type="time"
+                          className="form-control"
+                          onChange={(e) => {
+                            handleTimeChange(
+                              e,
+                              location._id,
+                              index,
+                              "departure"
+                            );
+                          }}
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="d-flex flex-column align-items-start">
-                  <ArrowDown size={30} className="my-2" />
-                </div>
-              </>
-            );
-          }
-        })}
+                  <div className="d-flex flex-column align-items-start">
+                    <ArrowDown size={30} className="my-2" />
+                  </div>
+                </>
+              );
+            }
+          })}
 
         <hr />
 
@@ -115,12 +294,32 @@ const AddNewBus = ({ handleClose, show }) => {
             <div className="d-flex flex-row justify-items-center w-100 gap-2">
               <div className="w-100 d-flex align-items-center flex-row">
                 <div className="w-50">From:</div>
-                <input type="date" className="form-control" />
+                <input
+                  type="date"
+                  className="form-control"
+                  onChange={(e) => {
+                    if (!e.target.value) {
+                      setPeriodOperatingFrom(null);
+                      return;
+                    }
+                    setPeriodOperatingFrom(e.target.value);
+                  }}
+                />
               </div>
 
               <div className="w-100 d-flex align-items-center flex-row">
                 <div className="w-50">To:</div>
-                <input type="date" className="form-control" />
+                <input
+                  type="date"
+                  className="form-control"
+                  onChange={(e) => {
+                    if (!e.target.value) {
+                      setPeriodOperatingTo(null);
+                      return;
+                    }
+                    setPeriodOperatingTo(e.target.value);
+                  }}
+                />
               </div>
             </div>
           </div>
@@ -130,35 +329,23 @@ const AddNewBus = ({ handleClose, show }) => {
           <div className="mb-3 d-flex justify-content-start align-items-start flex-row gap-2">
             <div className="w-50 fw-semibold">Recurring:</div>
             <div className="w-100">
-              <div className="d-flex flex-row gap-2 align-items-center">
-                <input type="checkbox" checked />
-                <span>Every Monday</span>
-              </div>
-
-              <div className="d-flex flex-row gap-2 align-items-center">
-                <input type="checkbox" checked />
-                <span>Every Tuesday</span>
-              </div>
-              <div className="d-flex flex-row gap-2 align-items-center">
-                <input type="checkbox" checked />
-                <span>Every Wednesday</span>
-              </div>
-              <div className="d-flex flex-row gap-2 align-items-center">
-                <input type="checkbox" checked />
-                <span>Every Thursday</span>
-              </div>
-              <div className="d-flex flex-row gap-2 align-items-center">
-                <input type="checkbox" checked />
-                <span>Every Friday</span>
-              </div>
-              <div className="d-flex flex-row gap-2 align-items-center">
-                <input type="checkbox" checked />
-                <span>Every Saturday</span>
-              </div>
-              <div className="d-flex flex-row gap-2 align-items-center">
-                <input type="checkbox" checked />
-                <span>Every Sunday</span>
-              </div>
+              {recurring.map((day, index) => {
+                return (
+                  <div
+                    className="d-flex flex-row gap-2 align-items-center"
+                    key={day.id}
+                  >
+                    <input
+                      type="checkbox"
+                      checked={day.checked}
+                      onChange={(e) => {
+                        handleRecurringCheckbox(e.target.value, day);
+                      }}
+                    />
+                    <span>Every {day.name}</span>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
